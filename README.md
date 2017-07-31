0e16144e0d8169751a1294cd11c999ad4ec40b6a
Tai <taitrananhvn@gmail.com>
2017-07-30 17:03:26 -0700

    add file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # python
+installation
+https://www.tutorialspoint.com/python/python_environment.htm
+
---
+++ hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# run as below
+# chmod +x hello.py
+# ./hello.py or python hello.py
+
+print "Hello, Python!"
+
+for x in range(0, 3):
+    print "value:%d" % (x)
